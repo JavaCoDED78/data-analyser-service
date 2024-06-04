fdf1a64b1ba7d6560e5185dbee86e654759ad963
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-06-04 11:10:30 +0300

    [Feature] Add Dockerfile

--- src/main/java/com/javaded/config/BeanConfig.java
+++ src/main/java/com/javaded/config/BeanConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
+import java.util.Objects;
 
 @Configuration
 public class BeanConfig {
@@ -15,7 +16,7 @@ public class BeanConfig {
     @Bean
     public XML consumerXml() {
         return new XMLDocument(
-                new File("src/main/resources/kafka/consumer.xml")
+                Objects.requireNonNull(getClass().getResourceAsStream("/kafka/consumer.xml")).readAllBytes()
         );
     }
 
